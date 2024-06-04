a5b8f6da0c61d5bea76c4846e8686443282bfd42
arsany199 <arsanykamal30@gmail.com>
2024-06-04 06:19:44 -0700

    8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  const strings = [];
+
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((i) => {
+    if (typeof i === 'string' && i.startsWith(startString)) {
+      strings.push(i.slice(startString.length));
+    }
+  });
+  return strings.join('-');
+};
