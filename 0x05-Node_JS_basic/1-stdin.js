44bbd5a9d6b5fee286893e22e48a3406406f5e38
arsany199 <arsanykamal30@gmail.com>
2024-07-22 15:22:10 -0700

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-readline.question('Welcome to Holberton School, what is your name? \n', (name) => {
-  console.log(`Your name is: ${name}`);
-
-  readline.close();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
