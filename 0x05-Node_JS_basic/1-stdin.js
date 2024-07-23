62ece1bf67863515dd58531c7b08893a4d9495fa
arsany199 <arsanykamal30@gmail.com>
2024-07-23 04:43:43 -0700

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('mydata', (mydata) => {
+process.stdin.on('data', (mydata) => {
   process.stdout.write(`Your name is: ${mydata}`);
 });
 
