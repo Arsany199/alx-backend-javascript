5c50cdc371a7132303760d79abfd0836a30e36ba
arsany199 <arsanykamal30@gmail.com>
2024-07-23 02:22:06 -0700

    4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const localhost = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, localhost, () => {
+  console.log(`Server running at http://${localhost}:${port}`);
+});
+
+module.exports = app;
