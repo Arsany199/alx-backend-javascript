d9f4c9227dd5ca8c96106d33fd8144b4693a9b02
arsany199 <arsanykamal30@gmail.com>
2024-07-25 05:48:41 -0700

    3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,6 +1,6 @@
 const Utils = require('./utils');
 
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${totalCost}`);
 };
