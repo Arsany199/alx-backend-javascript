2ab6238df2d84992c75fe7d096160a0d3872eb98
arsany199 <arsanykamal30@gmail.com>
2024-07-25 15:45:47 -0700

    8-api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const PORT = 7865;
+const myport = 7865;
 
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(myport, () => {
+  console.log(`API available on localhost port ${myport}`);
 });
 
 module.exports = app;
