127f78f2dcaf2c305028f148abdad675ae141142
arsany199 <arsanykamal30@gmail.com>
2024-07-25 04:51:45 -0700

    1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,16 +1,16 @@
 function calculateNumber(type, a, b) {
-	if (type === SUM) {
-	    	return Math.round(a) + Math.round(b);
-	}
-	if (type === SUBTRACT) {
-		return Math.round(a) - Math.round(b);
-	}
-	if (type === DIVIDE) {
-		if (Math.round(b) === 0) {
-			return 'Error';
-		}
-		return Math.round(a) / Math.round(b);
-	}
+  if (type === SUM) {
+    return math.round(a) + math.round(b);
+  }
+  if (type === SUBTRACT) {
+    return math.round(a) - math.round(b);
+  }
+  if (type === DIVIDE) {
+    if (math.round(b) === 0) {
+      return 'Error';
+    }
+    return math.round(a) / math.round(b);
+  }
 }
 
 module.exports = calculateNumber;
